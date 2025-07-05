c6c25553572f4f46adb4b3c8d9a52d8fd5071348
Vitor <vitorsena78@gmail.com>
2025-07-05 11:00:12 -0300

    adição de campos de controle created_at e updated_at

--- Banco de dados IBG.sql
+++ Banco de dados IBG.sql
@@ -7,7 +7,7 @@ CREATE TABLE Paciente (
   cpf VARCHAR(45) NULL,
   sus VARCHAR(45) NULL,
   telefone VARCHAR(45) NULL,
-  endere�o TEXT NULL,
+  endereço TEXT NULL,
   pa_x_mmhg VARCHAR(20) NULL,
   fc_bpm FLOAT NULL,
   fr_ibpm FLOAT NULL,
@@ -17,12 +17,16 @@ CREATE TABLE Paciente (
   peso FLOAT NULL,
   altura FLOAT NULL,
   imc FLOAT NULL,
+  created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(id)
 );
 
 CREATE TABLE Especialidade (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   nome VARCHAR(45) NOT NULL,
+  created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(id)
 );
 
@@ -30,6 +34,7 @@ CREATE TABLE Paciente_has_Especialidade (
   Paciente_id INTEGER UNSIGNED NOT NULL,
   Especialidade_id INTEGER UNSIGNED NOT NULL,
   data_atendimento DATE NULL DEFAULT (CURRENT_DATE),
+  created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(Paciente_id, Especialidade_id),
   INDEX Paciente_has_Especialidade_FKIndex1(Paciente_id),
   INDEX Paciente_has_Especialidade_FKIndex2(Especialidade_id),
